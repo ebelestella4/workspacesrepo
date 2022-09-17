1cb3a2478680a12c49b7572502607462def1ae1e
ebelestella4 <ebelestella4@gmail.com>
2022-09-16 17:27:02 -0700

    modified REST API

---
+++ jindu1.py
@@ -0,0 +1,10 @@
+Resources "aws_instance" "Test1" {
+        count = 4
+        AMI = ami-05fa00d4c63e32376
+        instance_type = t2.micro
+        key_name = 
+        VPC_security_groups_ids = "Fyneby-newkeypair"
+
+
+Never in my life would I have thought i would go into ID after really loving business. I would have been an accountant but nope debit and credit really gets hold at some point. but well am happy i did this big move i am really enjoing my new profession. i just want to get a job and change have a new move as soon as possible. i could writee this for the either day because it is not easy. 
+
